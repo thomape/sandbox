5fccfd4bb37c5794180c54a118519e850a7eca85
Thomas Errington <tom.errington58@gmail.com>
2022-10-05 12:46:39 -0400

    Removed some unnecessary files

--- backend/schemas/test_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class ContactSchema(BaseModel):
-    id: Optional[int] = None
-    first_name: str
-    last_name: str
-    email: Optional[str] = None
